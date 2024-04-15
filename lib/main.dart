11a5a8058f2ff3e49c46f095d58127ed82e61592
kca2250 <2250katok@gmail.com>
2024-04-15 09:43:47 +0900

    環境変数を読み込む設定とstatusbarなどの色を設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kansai_no_obachat/app.dart';
 
-void main() {
-  runApp(const App());
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.deepOrange[400],
+      systemNavigationBarColor: Colors.white,
+    ),
+  );
+
+  runApp(
+    const App(),
+  );
 }
