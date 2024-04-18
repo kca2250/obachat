8368f804835f3db9165e6b18e8c7cf2c0c46dda1
kca2250 <2250katok@gmail.com>
2024-04-18 16:33:01 +0900

    firebaseを導入

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:kansai_no_obachat/app.dart';
+import 'package:kansai_no_obachat/firebase_options.dart';
 
 Future<void> main() async {
   await dotenv.load(fileName: '.env');
@@ -11,6 +14,10 @@ Future<void> main() async {
       statusBarColor: Colors.deepOrange[400],
       systemNavigationBarColor: Colors.white,
     ),
+  );
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
 
   runApp(
